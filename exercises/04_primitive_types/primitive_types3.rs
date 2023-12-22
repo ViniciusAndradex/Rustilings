c715e469c9aa996c64e76f8d5957c2589a420fae
Vinicius Andrade <viniciusandradeprog@gmail.com>
2023-12-21 23:52:52 -0300

    primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,8 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a: [i32; 8] = [0; 8];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
